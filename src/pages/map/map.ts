80420c3726b51f6deaf80980577e8accb4608a7b
bollen <david.boll@me.com>
2018-03-06 12:28:26 +0100

    Adds markers to map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -12,6 +12,24 @@ export class MapPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
+  myPlaces = [
+    {
+      'name': 'Friends Arena',
+      'lat': '59.3727031',
+      'lng': '18.000231100000065'
+    },
+    {
+      'name': 'Home',
+      'lat': '59.3156866',
+      'lng': '18.03843219999999'
+    },
+    {
+      'name': 'Craft Academy, Stockholm',
+      'lat': '59.34980649999999',
+      'lng': '18.070664599999986'
+    }
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -28,7 +46,20 @@ export class MapPage {
     }
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    
+    for (let place of this.myPlaces) {
+      this.addMarker(place, place['name'])
+    }
+  }
 
+  addMarker(posInfo, info) {
+    let postition = new google.maps.LatLng(posInfo['lat'], posInfo['lng']);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: postition
+    });
   }
 
 }
